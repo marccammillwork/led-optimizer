165e16717a3f7ef3cae9f723c39242d631070b5b
marccammillwork <marc@cammillwork.com>
2025-05-01 07:58:54 -0400

    Update led_optimizer_web.py

--- led_optimizer_web.py
+++ led_optimizer_web.py
@@ -10,7 +10,7 @@ strip_options = {59: 43.26, 118: 74.63, 236: 139.06}
 power_specs = [
     {'W': 36, 'cost': 26.16},
     {'W': 60, 'cost': 82.72},
-    {'W': 96, 'cost': 93.91},
+    {'W': 96, 'cost': 43.04},
 ]
 power_specs.sort(key=lambda s: s['cost']/s['W'])
 
